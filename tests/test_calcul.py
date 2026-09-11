6a41f4fac58d68a88e3da51c0d15f12223636294
SlowlyTv <116349579+SlowlyTv@users.noreply.github.com>
2026-09-11 11:54:51 +0000

    wip tests

--- tests/test_calcul.py
+++ tests/test_calcul.py
@@ -5,5 +5,8 @@ class TestAddition(unittest.TestCase):
     def test_positive(self):
         self.assertEqual(addition(2, 3), 5)
 
+    def test_negative(self):
+        self.assertEqual(addition(-2, 3), 1)
+
 if __name__ == '__main__':
     unittest.main()
