b48bd9192f1825208bdcc1c1366f0c2dd8d90a78
SlowlyTv <116349579+SlowlyTv@users.noreply.github.com>
2026-09-11 11:54:32 +0000

    test a reformuler

---
+++ tests/test_calcul.py
@@ -0,0 +1,9 @@
+import unittest
+from src.calcul import addition
+
+class TestAddition(unittest.TestCase):
+    def test_positive(self):
+        self.assertEqual(addition(2, 3), 5)
+
+if __name__ == '__main__':
+    unittest.main()
